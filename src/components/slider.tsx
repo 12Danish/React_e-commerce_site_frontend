cd6ee59906e5bcb0ded96df77f5836bee20fd9ac
12Danish <danishabbas2004@gmail.com>
2023-11-20 22:26:32 +0500

    refactored slider

--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -99,9 +99,52 @@ const Desc = styled.p`
   font-size: 30px;
 `;
 
-
 // This is the main component which is returned
 const Slider = () => {
+  // sliderIndex is the state value and handleClick is function
+  const { sliderIndex, handleClick } = useSlideMovement();
+  return (
+    <Container>
+      {/* This component contains the left arrow and I need to remove hard coding here */}
+      <Arrow direction="left" onClick={() => handleClick("left", 3)}>
+        {/* This is being imported from mui */}
+        <ArrowBackIosIcon sx={{ color: "#f8f8ff", fontSize: 30 }} />
+      </Arrow>
+      {/* This wrapper wraps around all of the slides */}
+      <Wrapper slideindex={sliderIndex}>
+        {sliderInfo.map((item) => (
+          <Slide key={item.id} bgcolor={item.bg}>
+            {/* This wraps around the image*/}
+            <ImageWrapper>
+              {/* Linking the image over here  */}
+              <Image src={item.img} />
+            </ImageWrapper>
+            {/* This wraps around all of the info*/}
+            <InfoWrapper>
+              <Title color={item.h1Color}>{item.title}</Title>
+              <Desc>{item.desc}</Desc>
+              <Button
+                bgcolor="#191970"
+                fontcolor="white"
+                bordercolor="white"
+                width="auto"
+              >
+                {item.button_name}
+              </Button>
+            </InfoWrapper>
+          </Slide>
+        ))}
+      </Wrapper>
+      {/* This wraps around the right arrow  */}
+      <Arrow direction="right" onClick={() => handleClick("right", 3)}>
+        <ArrowForwardIosIcon sx={{ color: "#f8f8ff", fontSize: 30 }} />
+      </Arrow>
+    </Container>
+  );
+};
+
+// This function handles the state and the logic for the slide movement
+function useSlideMovement() {
   const [sliderIndex, setSliderIndex] = useState(0);
   // This function takes the direction(left or right) as argument and sets the value of slider index accordingly
   const handleClick = (direction: string, maxSlides: number) => {
@@ -130,37 +173,7 @@ const Slider = () => {
       }
     }
   };
-  return (
-    <Container>
-      {/* This component contains the left arrow and I need to remove hard coding here */}
-      <Arrow direction="left" onClick={() => handleClick("left", 3)}>
-        {/* This is being imported from mui */}
-        <ArrowBackIosIcon sx={{ color: "#f8f8ff", fontSize: 30 }} />
-      </Arrow>
-      {/* This wrapper wraps around all of the slides */}
-      <Wrapper slideindex={sliderIndex}>
-        {sliderInfo.map((item) => (
-          <Slide key={item.id} bgcolor={item.bg}>
-            {/* This wraps around the image*/}
-            <ImageWrapper>
-              {/* Linking the image over here  */}
-              <Image src={item.img} />
-            </ImageWrapper>
-            {/* This wraps around all of the info*/}
-            <InfoWrapper>
-              <Title color={item.h1Color}>{item.title}</Title>
-              <Desc>{item.desc}</Desc>
-              <Button bgcolor="#191970" fontcolor="white" bordercolor="white" width= "auto">{item.button_name}</Button>
-            </InfoWrapper>
-          </Slide>
-        ))}
-      </Wrapper>
-      {/* This wraps around the right arrow  */}
-      <Arrow direction="right" onClick={() => handleClick("right", 3)}>
-        <ArrowForwardIosIcon sx={{ color: "#f8f8ff", fontSize: 30 }} />
-      </Arrow>
-    </Container>
-  );
-};
+  return { sliderIndex, handleClick };
+}
 
 export default Slider;
